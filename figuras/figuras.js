76382f5232e6ac0c912c4b76a74ce78ba8aac5ae
Francisco García <francoffee57@gmail.com>
2021-08-12 22:08:37 -0500

    Funcionalidad del la página terminada

--- figuras/figuras.js
+++ figuras/figuras.js
@@ -18,29 +18,62 @@ const areaTriangle = (base, altura) =>{
     return `El área del triangulo es: ${aria} `
 }
 
-//Calcular el Circulo
+//Calcular el Círculo
 const pi = Math.PI
 const perimeterCircle = (radio) =>{
     let perimeter = ((2 * radio) * pi)
-    return `El perímetro del circulo es: ${perimeter} `
+    return `El perímetro del círculo es: ${perimeter} `
 }
 const ariaCircle = (radio) =>{
     let aria = (pi * (radio ** 2)) 
-    return `El área del circulo es: ${aria} `
+    return `El área del círculo es: ${aria} `
 }
 
-//Conexión con HTML
+//--Conexión con HTML
+//cuadrado
 function calcularPerimetroCuadrado(){
     const input = document.getElementById("InputSquare")
     const value = input.value
 
     alert( perimeterSquare(value) + "unidades")
 }
-
 function calcularAreaCuadrado(){
     const input = document.getElementById("InputSquare")
     const value = input.value
 
     alert( ariaSquare(value) + "unidades^2")
 }
+//Tríangulo
+function calcularPerimetroTriangulo(){
+    const input1 = document.getElementById("InputTriangleL1")
+    const l1 = parseFloat(input1.value)
 
+    const input2 = document.getElementById("InputTriangleL2")
+    const l2 = parseFloat(input2.value)
+
+    const input3 = document.getElementById("InputTriangleBase")
+    const base = parseFloat(input3.value)
+
+    alert( perimeterTriangle(l1, l2, base) + "unidades")
+}
+function calcularAreaTriangulo(){
+    const input3 = document.getElementById("InputTriangleBase")
+    const base = parseFloat(input3.value)
+
+    const input4 = document.getElementById("InputTriangleHeight")
+    const altura = parseFloat(input4.value)
+    alert( areaTriangle(base, altura) + "unidades^2")
+}
+//Círculo
+function calcularPerimetrocirculo(){
+    const input = document.getElementById("InputCircle")
+    const value = input.value
+
+    alert( perimeterCircle(value) + "unidades")
+}
+function calcularAreacirculo(){
+    const input = document.getElementById("InputCircle")
+    const value = input.value
+
+    alert( ariaCircle(value) + "unidades^2")
+}
