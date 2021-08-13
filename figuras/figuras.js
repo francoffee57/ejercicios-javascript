88633756ac8d3fa789ff2b4fd2dea43511631ac6
Francisco García <francoffee57@gmail.com>
2021-08-12 21:09:00 -0500

    Estructura HTML del cuadrado. Conexión con javascript y separe las funciones de área y perímetro

--- figuras/figuras.js
+++ figuras/figuras.js
@@ -1,25 +1,46 @@
-
-const calSquare= (square) =>{
+//Calcuar el Cuadrado
+const perimeterSquare= (square) =>{
     let perimeter = square * 4
+    return `El perímetro del cuadrado es: ${perimeter} `
+}
+const ariaSquare= (square) =>{
     let aria = square ** 2
-
-    return `El perimetro del cuadrado es: ${perimeter}. El area es ${aria}`
+    return `El área del cuadrado es ${aria} `
 }
 
-const calTriangle = (l1, l2, base, altura) =>{
+//Callcular el Triangulo
+const perimeterTriangle = (l1, l2, base) =>{
     let perimeter = (l1 + l2 + base)
+    return `El perímetro del triangulo es: ${perimeter} `
+}
+const areaTriangle = (base, altura) =>{
     let aria = (base * altura) / 2
-
-    return `El perimetro del triangulo es: ${perimeter}. El area es ${aria}`
+    return `El área del triangulo es: ${aria} `
 }
 
-const calCircle = (radio) =>{
-    const pi = 3.1416
-    let perimeter = ((pi * radio) * 2)
+//Calcular el Circulo
+const pi = Math.PI
+const perimeterCircle = (radio) =>{
+    let perimeter = ((2 * radio) * pi)
+    return `El perímetro del circulo es: ${perimeter} `
+}
+const ariaCircle = (radio) =>{
     let aria = (pi * (radio ** 2)) 
-
-    return `El perimetro del circulo es: ${perimeter}. El area es ${aria}`
+    return `El área del circulo es: ${aria} `
 }
-console.log(calSquare(4))
-console.log(calTriangle(16.16, 16.16, 12, 15))
-console.log(calCircle(2))
+
+//Conexión con HTML
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputSquare")
+    const value = input.value
+
+    alert( perimeterSquare(value) + "unidades")
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputSquare")
+    const value = input.value
+
+    alert( ariaSquare(value) + "unidades^2")
+}
+
