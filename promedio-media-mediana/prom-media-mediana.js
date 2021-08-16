2f035794263b22fd7a401eb36d764979bd22f89c
Francisco García <francoffee57@gmail.com>
2021-08-15 20:27:57 -0500

    Quite los alert y ahora el resultado se muestra en pantalla

---
+++ promedio-media-mediana/prom-media-mediana.js
@@ -0,0 +1,12 @@
+const calPromedio = (lista)=>{
+    let total = 0
+    
+    for(let i = 0; i < lista.length; i++) {
+        total += lista[i];
+    }    
+    
+    return total / lista.length
+} 
+
+
+
